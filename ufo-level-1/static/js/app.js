ffcf67f348337629da048942b04093e95ab7f4f9
dorisakpovwa <75711579+dorisakpovwa@users.noreply.github.com>
2021-04-02 22:46:25 -0400

    added table

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the UFO data from data.js
+// console.log(data);
+
+ // Step 1: Loop Through `ufoData` and console.log each UFO object
+ // Step 2:  Use d3 to append one table row `tr` for each UFO object
+ufoData.forEach((item) => {
+    var row = tbody.append("tr");
+    row.append("td").text(item["datetime"]);
+    row.append("td").text(item["city"]);
+    row.append("td").text(item["state"]);
+    row.append("td").text(item["country"]);
+    row.append("td").text(item["shape"]);
+    row.append("td").text(item["durationMinutes"]);
+    row.append("td").text(item["comments"]);
+
+   console.log(item);
+ });
+
+ //  console.log(item);
+
