39f56662e818dfc349a1ff3952c912d21d48bafe
dorisakpovwa <75711579+dorisakpovwa@users.noreply.github.com>
2021-04-04 22:13:29 -0400

    added filter

--- ufo-level-1/static/js/app.js
+++ ufo-level-1/static/js/app.js
@@ -7,7 +7,30 @@ var form = d3.select ("#form");
 // create event handlers
 button.on("click", runEnter);
 form.on("submit", runEnter);
+
+var filteredData = null;
+// select table
+var tbody = d3.select("tbody");
 // complete the event handler function for the form
+function Buildtable(data) { 
+
+ tbody.html("")
+  // Console.log the UFO data from data.js
+   console.log(data);
+   // Step 1: Loop Through `ufoData` and console.log each UFO object
+   // Step 2:  Use d3 to append one table row `tr` for each UFO object
+  data.forEach((item) => {
+      var row = tbody.append("tr");
+      row.append("td").text(item["datetime"]);
+      row.append("td").text(item["city"]);
+      row.append("td").text(item["state"]);
+      row.append("td").text(item["country"]);
+      row.append("td").text(item["shape"]);
+      row.append("td").text(item["durationMinutes"]);
+      row.append("td").text(item["comments"]);
+      console.log(item);
+   });
+  }
 function runEnter() {
 // prevent the page from refreshing
 d3.event.preventDefault();
@@ -25,26 +48,10 @@ function dates(datelist) {
   }
   return result
 }
-var filteredData = ufoData.filter(ufoData => ufoData.datetime === inputValue);
+filteredData = ufoData.filter(ufoData => ufoData.datetime === inputValue);
 console.log(filteredData);
+
+Buildtable(filteredData);
 }
 // Get a reference to the table body
-var tbody = d3.select("tbody");
-// Console.log the UFO data from data.js
- console.log(ufoData);
- // Step 1: Loop Through `ufoData` and console.log each UFO object
- // Step 2:  Use d3 to append one table row `tr` for each UFO object
-ufoData.forEach((item) => {
-    var row = tbody.append("tr");
-    row.append("td").text(item["datetime"]);
-    row.append("td").text(item["city"]);
-    row.append("td").text(item["state"]);
-    row.append("td").text(item["country"]);
-    row.append("td").text(item["shape"]);
-    row.append("td").text(item["durationMinutes"]);
-    row.append("td").text(item["comments"]);
-    console.log(item);
- });
-
- //  console.log(item);
-
+Buildtable(ufoData);
